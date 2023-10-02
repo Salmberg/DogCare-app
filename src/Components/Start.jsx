5d7f5ae4f9bd0f09d7b80da1b474426365308da7
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-10-02 17:48:50 +0200

    Formatted code

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -1,56 +1,56 @@
 import React from 'react';
-import '../Start.css'; 
+import '../Start.css';
 
 const Start = (props) => {
     return (
-      <section className="start-section">
-        <div className="start-grid">
-          
+        <section className="start-section">
+            <div className="start-grid">
 
-          <div className="grid-item">
-            <div className="item-content">
-              <div className="image-box">
-                <img src="UteBild.jpeg" alt="Image 1" />
-              </div>
-              <div className="text-box">
-                <p>Golden Dogs är mer än bara ett hunddagis. Det är en fristad för våra fyrbenta vänner, en plats där de kan leka, lära sig och trivas medan du är borta. Vi förstår att din hund är en viktig medlem av din familj, och därför strävar vi efter att erbjuda den bästa omsorgen och upplevelsen för varje hund som går genom våra dörrar.</p>
-              </div>
-            </div>
-          </div>
 
-          <div className="grid-item">
-            <div className="item-content">
-             <div className="text-box">
-              <p>Vår anläggning är utrustad med toppmodern teknik för att säkerställa en säker och bekväm vistelse för våra gäster. Vi har stora lekytor inomhus och utomhus där hundar kan utforska och ha kul. Vi har också särskilda områden för vila och avskildhet för dem som behöver en paus från all aktivitet.</p>
-            </div>
-            <div className="image-box">
-            <img src="InneBild.jpeg" alt="Image 1" />
-             </div>
+                <div className="grid-item">
+                    <div className="item-content">
+                        <div className="image-box">
+                            <img src="UteBild.jpeg" alt="Image 1" />
+                        </div>
+                        <div className="text-box">
+                            <p>Golden Dogs är mer än bara ett hunddagis. Det är en fristad för våra fyrbenta vänner, en plats där de kan leka, lära sig och trivas medan du är borta. Vi förstår att din hund är en viktig medlem av din familj, och därför strävar vi efter att erbjuda den bästa omsorgen och upplevelsen för varje hund som går genom våra dörrar.</p>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="grid-item">
+                    <div className="item-content">
+                        <div className="text-box">
+                            <p>Vår anläggning är utrustad med toppmodern teknik för att säkerställa en säker och bekväm vistelse för våra gäster. Vi har stora lekytor inomhus och utomhus där hundar kan utforska och ha kul. Vi har också särskilda områden för vila och avskildhet för dem som behöver en paus från all aktivitet.</p>
+                        </div>
+                        <div className="image-box">
+                            <img src="InneBild.jpeg" alt="Image 1" />
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="grid-item">
+                    <div className="item-content">
+                        <div className="image-box">
+                            <img src="VilaBild.jpeg" alt="Image 1" />
+                        </div>
+                        <div className="text-box">
+                            <p>På Hundens Paradis är vår filosofi enkel - att skapa en kärleksfull och trygg miljö där din hund kan trivas och växa. Vi strävar efter att bli en förlängning av din egen familj och ge din hund den kärlek och omsorg den förtjänar.
+
+                                Så när du behöver en pålitlig plats att ta hand om din hund när du är borta, tveka inte att kontakta oss på Hundens Paradis. Din hund kommer att bli en del av vår familj och njuta av en underbar tid fylld med lek, kärlek och äventyr.
+
+                                Kom och besök oss idag och upplev skillnaden på Golden Dogs!
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
-
-
-          <div className="grid-item">
-            <div className="item-content">
-              <div className="image-box">
-                <img src="VilaBild.jpeg" alt="Image 1" />
-              </div>
-              <div className="text-box">
-                <p>På Hundens Paradis är vår filosofi enkel - att skapa en kärleksfull och trygg miljö där din hund kan trivas och växa. Vi strävar efter att bli en förlängning av din egen familj och ge din hund den kärlek och omsorg den förtjänar.
-
-Så när du behöver en pålitlig plats att ta hand om din hund när du är borta, tveka inte att kontakta oss på Hundens Paradis. Din hund kommer att bli en del av vår familj och njuta av en underbar tid fylld med lek, kärlek och äventyr.
-
-Kom och besök oss idag och upplev skillnaden på Golden Dogs!
-
-</p>
-              </div>
-            </div>
-          </div>
-
-          
-        </div>
-      </section>
+        </section>
     );
-  }
+}
 
 export default Start;
