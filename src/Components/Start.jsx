55bc5f9f039be2d886ed9d84fd823c072d1b6d82
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-10-04 11:34:42 +0200

    new import of the images

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../Start.css';
+import UteBild from '../images/UteBild.jpeg';
+import InneBild from '../images/InneBild.jpeg';
+import VilaBild from '../images/VilaBild.jpeg';
+
 
 const Start = (props) => {
     return (
@@ -10,7 +14,7 @@ const Start = (props) => {
                 <div className="grid-item">
                     <div className="item-content">
                         <div className="image-box">
-                            <img src="../src/images/UteBild.jpeg" alt="Image 1" />
+                            <img src={UteBild} alt="Image 1" />
                         </div>
                         <div className="text-box">
                             <p>Golden Dogs är mer än bara ett hunddagis. Det är en fristad för våra fyrbenta vänner, en plats där de kan leka, lära sig och trivas medan du är borta. Vi förstår att din hund är en viktig medlem av din familj, och därför strävar vi efter att erbjuda den bästa omsorgen och upplevelsen för varje hund som går genom våra dörrar.</p>
@@ -24,7 +28,7 @@ const Start = (props) => {
                             <p>Vår anläggning är utrustad med toppmodern teknik för att säkerställa en säker och bekväm vistelse för våra gäster. Vi har stora lekytor inomhus och utomhus där hundar kan utforska och ha kul. Vi har också särskilda områden för vila och avskildhet för dem som behöver en paus från all aktivitet.</p>
                         </div>
                         <div className="image-box">
-                            <img src="InneBild.jpeg" alt="Image 1" />
+                            <img src={InneBild} alt="Image 1" />
                         </div>
                     </div>
                 </div>
@@ -33,7 +37,7 @@ const Start = (props) => {
                 <div className="grid-item">
                     <div className="item-content">
                         <div className="image-box">
-                            <img src="VilaBild.jpeg" alt="Image 1" />
+                            <img src={VilaBild} alt="Image 1" />
                         </div>
                         <div className="text-box">
                             <p>På Hundens Paradis är vår filosofi enkel - att skapa en kärleksfull och trygg miljö där din hund kan trivas och växa. Vi strävar efter att bli en förlängning av din egen familj och ge din hund den kärlek och omsorg den förtjänar.
